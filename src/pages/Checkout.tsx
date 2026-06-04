ae3fe694a4f92738282547766c0ed97685432be3
email2ishmum <103054997+email2ishmum@users.noreply.github.com>
2026-06-04 20:11:28 +0600

    Refactor useEffect to use React namespace

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -29,7 +29,7 @@ export default function Checkout() {
     }
   }, [finalTotal]);
   
-  useEffect(() => {
+  React.useEffect(() => {
     if (items.length === 0 && !isSuccess) {
       navigate('/');
     }
