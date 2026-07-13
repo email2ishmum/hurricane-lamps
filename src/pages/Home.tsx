7cbf3203314de2cdc834dcdd6388b21bd2b4e1a3
Fyaz Bin Yousuf <103054997+email2ishmum@users.noreply.github.com>
2026-07-14 00:56:35 +0600

    hero corp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ export default function Home() {
       </div>
 
       {/* Hero Section */}
-      <section className="relative h-[90vh] overflow-hidden bg-brand-black">
+      <section className="relative h-[45vh] md:h-[55vh] lg:h-[65vh] overflow-hidden bg-brand-black">
         <motion.div 
           initial={{ scale: 1.1, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
